2227c16bb868712c8959f926f1d24034e84ad513
Byron Davies <byron@martinamoo.com>
2023-02-15 16:40:19 +0000

    Initial Commit

---
+++ LottieTest/LottieTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieTestApp.swift
+//  LottieTest
+//
+//  Created by Byron Davies on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LottieTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
